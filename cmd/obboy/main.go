2f12a2ba2115236ce105b2e18f52e3d6b96e56fa
Abul Kalam Faruk <abulkalamfaruk@gmail.com>
2021-09-25 14:42:19 +0600

    starter

--- cmd/obboy/main.go
+++ cmd/obboy/main.go
@@ -8,14 +8,17 @@ import (
 )
 
 func main() {
+	// create the cobra command 
 	var obboyCmd = &cobra.Command {
 		Use: "obboy",
 		Short: "Obboy Blockchain Cli",
+		// it will run the following function when called
 		Run: func(cmd *cobra.Command, args []string) {
 
 		},
 	}
 
+	// This will execute the command in the terminal and catch any error and print it. 
 	err := obboyCmd.Execute()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
